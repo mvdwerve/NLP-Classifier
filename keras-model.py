8ee6838c01ee545d55365e812502bda4a9e8182d
Michael van der Werve <mvdwerve@outlook.com>
2017-12-17 17:19:51 +0100

    added initial models and such

--- keras-model.py
+++ keras-model.py
@@ -3,41 +3,28 @@ from keras.models import Sequential
 from keras.layers import Dense, Input
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
+from keras.utils.vis_utils import plot_model
 
 X = np.load("data/data.npy")
 y = to_categorical(np.load("data/targets.npy"))
 
 X = np.array([row.flatten() for row in X])
-results =[]
-vresults =[]
-reserr = []
-vreserr = []
 
-a = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 3000]
-for i in a:
-    avg = []
-    vavg = []
-    for j in range(4):
-        model = Sequential()
-        model.add(Dense(i, input_dim=X.shape[1], activation='selu'))
-        model.add(Dense(2, input_dim=X.shape[1], activation='softmax'))
-        model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-        model.summary()
+model = Sequential()
+model.add(Dense(500, input_dim=X.shape[1], activation='selu'))
+model.add(Dense(2, input_dim=X.shape[1], activation='softmax'))
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
 
-        data = model.fit(X, y, epochs=10, validation_split=0.1).history
-        acc, valacc = data['acc'][-1], data['val_acc'][-1]
+data = model.fit(X, y, epochs=10, validation_split=0.1).history
 
-        avg.append(acc) 
-        vavg.append(valacc)
-
-    results.append(np.mean(avg))
-    vresults.append(np.mean(vavg))
-
-    reserr = np.std(avg)
-    vreserr = np.std(vavg)
-
-plt.errorbar(a, results, reserr, label='Training set')
-plt.errorbar(a, vresults, vreserr, label='Testing set')
+for layer in model.layers:
+    im = layer.get_weights()[0]
+    plt.imshow(X @ im, cmap=plt.cm.seismic)
+    plt.title("Visualisation of Neural Network weights")
+    plt.ylabel("Flattened hidden layer input node")
+    plt.xlabel("Hidden layer output neuron")
+    plt.show()
 
 plt.xlabel('Hidden Layer Units')
 plt.ylabel('Accuracy')
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ args = parser.parse_args()
 
 # make the dictionaries, tags and words
 pos_dict = {}
-pos_tags = {}
+pos_tags = {'NN': 0, 'NNS': 1}
 sentences = []
 target_words = []
 
@@ -33,9 +33,11 @@ with open('ted_dict_words_pos.txt') as f:
         for word in words:
             tagged_word = word.split('/')
             if len(tagged_word) > 1:
-                if pos_tags.get(tagged_word[1]) == None:
-                    pos_tags[tagged_word[1]] = len(pos_tags)
+                # if pos_tags.get(tagged_word[1]) == None:
+                #     pos_tags[tagged_word[1]] = len(pos_tags)
                 pos_dict[tagged_word[0]] = tagged_word[1]
+
+print(pos_tags)
 
 def get_pos_tag(index):
     return [k for (k, v) in list(pos_tags.items()) if v == index]
@@ -44,7 +46,7 @@ with open('ted_lm/to_run/data/train.txt') as f:
     content = f.readlines()
 
     for line in content:
-        if len(line.split(' ')) > 2:
+        if len(line.split(' ')) > 2 and pos_dict.get(line.split(' ')[-2]) in ['NN', 'NNS']:
             sentences.append(' '.join(line.split(' ')[:-2]))
             target_words.append(line.split(' ')[-2])
 
@@ -57,19 +59,23 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(1500, 500)
-        self.fc2 = nn.Linear(500, 45)
+        self.fc2 = nn.Linear(500, 2)
 
     def forward(self, x):
         x = F.selu(self.fc1(x))
         x = self.fc2(x)
-        return F.softmax(x)
+        return x
 
 # net = torch.load('diagnostic-classifier.pt', map_location=lambda storage, loc: storage)
 
 class Trainer:
-    def __init__(self, model, dictionary):
+    def __init__(self, model, dictionary, pt=None):
         # initialize vars for training the net
-        self.net = Net()
+        if pt == None:
+            self.net = Net()
+        else:
+            self.net = torch.load('diagnostic-classifier.pt', map_location=lambda storage, loc: storage)
+
         self.optimizer =  optim.SGD(self.net.parameters(), lr=args.lr, momentum=args.momentum)
         self.loss = nn.CrossEntropyLoss()
        
@@ -89,10 +95,10 @@ class Trainer:
     # NB Assumes the sentence is already tokenised!
     def tokenise(self, sentence):
         words = sentence.split(' ')
-        assert len(words) <= max_seq_len, "Sentence too long"
+        # assert len(words) <= max_seq_len, "Sentence too long"
         return torch.LongTensor([self.get_id(w) for w in words])
     
-    def evaluate(self, sentences, targets):
+    def evaluate(self, sentences, targets, train=True):
         # storing the output here
         outputs, hiddens = [], []
 
@@ -105,24 +111,30 @@ class Trainer:
             # Run the model, compute probabilities by applying softmax
             output, hidden = self.model(input_data, self.hidden)
 
-            # the prediction in the hidden layer
-            prediction = self.net(hidden[0])[1]
-            target_id = pos_tags.get(pos_dict.get(target))
-            
-            # append to the output and hiddens (faux batching)
-            outputs.append(output)
-            hiddens.append(hidden)
-    
-            # some user output
-            print('%s => %s' % (sentence, target))
-            if target_id == None:
-                print(" ---> skipped last sentence, because target \"%s\"not in dictionary" % target)
-                continue
+            if train:
+                # We need to train our model on the hidden state of the RNN
+                hidden_state = Variable(hidden[0].data, requires_grad=False)
+                # print('Prediction:', self.net.forward(hidden_state).cpu()[1].data.numpy())
+                prediction = self.net.forward(hidden_state)[1]
+                target_id = pos_tags.get(pos_dict.get(target))
 
-            # make the loss and optimizer step
-            loss = self.loss(prediction, Variable(torch.LongTensor([target_id])))
-            loss.backward()
-            self.optimizer.step()
+                # append to the output and hiddens (faux batching)
+                outputs.append(output)
+                hiddens.append(hidden)
+
+                # some user output
+                print('%s => %s --> %s' % (sentence, target, pos_dict.get(target)))
+                if target_id == None:
+                    print(" ---> skipped last sentence, because target \"%s\"not in dictionary" % target)
+                    continue
+
+                # make the loss and optimizer step
+                loss = self.loss(prediction, Variable(torch.LongTensor([target_id])))
+                loss.backward()
+                self.optimizer.step()
+            else:
+                outputs.append(output)
+                hiddens.append(hidden)
         
         # return the output
         return outputs, hiddens
@@ -133,22 +145,31 @@ def plot_tensor(tensors, indices):
     plt.show()
 
 # create a trainer and set batch size
+# trainer = Trainer(lm, dictionary, pt='diagnostic-classifier.pt')
 trainer = Trainer(lm, dictionary)
 
+print(sentences)
+
 # loop over the batches
-for i in range(0, len(sentences), args.batch_size):
-    print(i + 1, '/', len(sentences))
+for i in range(0, len(sentences[:2000]), args.batch_size):
+    print(i + 1, '/', len(sentences[:2000]))
     outputs, hiddens = trainer.evaluate(sentences[i:i+args.batch_size], target_words[i:i+args.batch_size])
 
 # save our own neural net
-torch.save(net, 'diagnostic-classifier.pt')
+torch.save(trainer.net, 'diagnostic-classifier.pt')
 
 print('Evaluating diagnostic classifier...')
+print(trainer.net)
 
-for i in range(0, len(sentences), batch_size):
-    _, hiddens = trainer.evaluate(sentences[i:i+args.batch_size], target_words[i:i+args.batch_size])
-    prediction = trainer.net(trainer.hidden[0]).data.numpy().tolist()[0]
-    print('Word to predict:', target_words[index])
-    print('Training sentence:', sentence)
-    print('Predicted POS tag:', get_pos_tag(prediction[0].index(max(prediction[0]))))
-    print('-------------------------')
+counter_correct = 0
+for i in range(2000, len(sentences), args.batch_size):
+    _, hiddens = trainer.evaluate(sentences[i:i+args.batch_size], target_words[i:i+args.batch_size], train=False)
+    hidden_states = list(zip(*hiddens))[0]
+    for j, hidden_state in enumerate(hidden_states):
+        prediction = list(trainer.net.forward(hidden_state).data.numpy()[1][0])
+        pred = get_pos_tag(prediction.index(max(prediction)))[0] == pos_dict.get(target_words[i + j])
+        if pred:
+            counter_correct += 1
+
+        print('Word to predict:', target_words[i + j], 'Predicted:', get_pos_tag(prediction.index(max(prediction))), 'Actual:', pos_dict.get(target_words[i + j]))
+    print('-----------', 100 * counter_correct / (i - 2000 + args.batch_size), '%------------')
